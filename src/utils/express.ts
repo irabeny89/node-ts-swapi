535f260ac3d4ef1fd3fe043a108524b39915f927
Ernest Irabor <irabeny89@gmail.com>
2021-09-28 04:06:48 +0100

    Refactor

--- src/utils/express.ts
+++ src/utils/express.ts
@@ -3,7 +3,7 @@ import morgan from "morgan";
 import compression from "compression";
 import helmet from "helmet";
 import cors from "cors";
-import moviesRoutes from "../routes/movies.routes"
+import moviesRoutes from "../routes/movies.routes";
 // express app instance
 const app = express();
 // helper middlewares
@@ -20,4 +20,4 @@ app.use((err: Errback, _req: Request, res: Response) => {
   if (err) return res.status(400).json(err);
 });
 
-export default app
+export default app;
